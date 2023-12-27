1ff03ccdd7b64676531e3e1479211782051d3c6e
casadeyahveh <154942940+casadeyahveh@users.noreply.github.com>
2023-12-26 22:43:42 -0600

    pagina de la clinica

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const jrbxmenu = document.querySelector(".menus")
+const menu = document.querySelector(".menu-navegacion")
+
+console.log(menu)
+console.log(jrbxmenu)
+
+ jrbxmenu.addEventListener("click", ()=>{
+    menu.classList.toggle("spread")
+ })
+
+ window.addEventListener("click", e=>{
+    if(menu.classList.contains("spread") && e.target != menu && e.target !=jrbxmenu   ){
+    
+        menu.classList.toggle("spread")
+
+    }
+ })
